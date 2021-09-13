95ffbb7e9358aef68befbcecb8afd99cfc162596
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-13 12:25:53 +0200

    adding best films

--- P6_03_js.js
+++ P6_03_js.js
@@ -1,13 +1,31 @@
+var image = document.getElementById('1')
+let bests = []
 
-const image = document.querySelector('#res')
-var variable = 2000
-var name_var = 'year'
-fetch(`http://127.0.0.1:8000/api/v1/titles/${name_var}=${variable}`)
-    .then(reponse => reponse.json())
-    .then(data => {console.log(data)})
+// Bests Movies
+arrow_left = document.getElementsById('arrow-left-best')
+arrow_right = document.getElementsById('arrow-right-best')
 
-year = 2001
-fetch(`http://127.0.0.1:8000/api/v1/titles/?year=${year}`)
+fetch(`http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score`)
     .then(reponse => reponse.json())
-    .then(data => {console.log(data)})
+    .then(data => {
+        for (let i = 0; i < 5; i++) {
+            bests.push(data.results[i].image_url)}
+        })
+    .then(next => {
+        fetch(`http://127.0.0.1:8000/api/v1/titles/?page=2&sort_by=-imdb_score`)
+        .then(reponse => reponse.json())
+        .then(data => {
+            for (let i = 0; i < 2; i++) {
+            bests.push(data.results[i].image_url)}
+    })
+        .then(func => {
+            for (let img_index = 0; img_index<4;img_index++){
+                var image = document.getElementById(`${img_index + 1}`)
+                image.src = bests[img_index]
+            }  
+
+
+
+        })
+    })
 
