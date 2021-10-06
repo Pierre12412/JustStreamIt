c43ee4916524eaa5c03e26e287da29650c1efdb6
Pierre BELLEGUEULE <78017882+Pierre12412@users.noreply.github.com>
2021-10-06 17:17:46 +0200

    Add files via upload

--- P6_03_js.js
+++ P6_03_js.js
@@ -2,47 +2,56 @@ const popup = document.getElementById("popup")
 const blur = document.getElementById("blur")
 const body = document.getElementsByTagName('body')
 
-let first = null
 
+//--------------------------------------------------Best--------------------------------------------------
 var image_best = [document.getElementById('1'),document.getElementById('2'),document.getElementById('3'),document.getElementById('4')]
-let bests = []
-let bests_title = []
+var bests = []
+var bests_title = []
 var index_first = 0
+arrow_left_best = document.getElementById('arrow-left-best')
+arrow_right_best = document.getElementById('arrow-right-best')
+var best_array = [bests,image_best,index_first,arrow_left_best,arrow_right_best,
+`http://localhost:8000/api/v1/titles/?sort_by=-imdb_score`,
+"http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score",bests_title]
 
-//Action
+//--------------------------------------------------Action--------------------------------------------------
 var image_action = [document.getElementById('5'),document.getElementById('6'),document.getElementById('7'),document.getElementById('8')]
-let action = []
-let action_title = []
+var action = []
+var action_title = []
 var index_action = 0
 arrow_left_action = document.getElementById('arrow-left-action')
 arrow_right_action = document.getElementById('arrow-right-action')
-let action_array = [action,image_action,index_action,arrow_left_action,arrow_right_action,`http://localhost:8000/api/v1/titles/?genre=action&sort_by=-imdb_score`,"http://localhost:8000/api/v1/titles/?genre=action&page=2&sort_by=-imdb_score",action_title]
 
-//Animation
+var action_array = [action,image_action,index_action,arrow_left_action,arrow_right_action,
+`http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score`,
+"http://localhost:8000/api/v1/titles/?genre=action&page=2&sort_by=-imdb_score",action_title]
+
+//--------------------------------------------------Animation--------------------------------------------------
 var image_animation = [document.getElementById('9'),document.getElementById('10'),document.getElementById('11'),document.getElementById('12')]
-let animation = []
-let animation_title = []
+var animation = []
+var animation_title = []
 var index_animation = 0
 arrow_left_animation = document.getElementById('arrow-left-animation')
 arrow_right_animation = document.getElementById('arrow-right-animation')
-let animation_array = [animation,image_animation,index_animation,arrow_left_animation,arrow_right_animation,`http://localhost:8000/api/v1/titles/?genre=animation&sort_by=-imdb_score`,"http://localhost:8000/api/v1/titles/?genre=animation&page=2&sort_by=-imdb_score",animation_title]
 
-//Thriller
+var animation_array = [animation,image_animation,index_animation,arrow_left_animation,arrow_right_animation,
+`http://localhost:8000/api/v1/titles/?genre=animation&sort_by=-imdb_score`,
+"http://localhost:8000/api/v1/titles/?genre=animation&page=2&sort_by=-imdb_score",animation_title]
+
+//--------------------------------------------------Thriller--------------------------------------------------
 var image_thriller = [document.getElementById('13'),document.getElementById('14'),document.getElementById('15'),document.getElementById('16')]
-let thriller = []
-let thriller_title = []
+var thriller = []
+var thriller_title = []
 var index_thriller = 0
 arrow_left_thriller = document.getElementById('arrow-left-thriller')
 arrow_right_thriller = document.getElementById('arrow-right-thriller')
-let thriller_array = [thriller,image_thriller,index_thriller,arrow_left_thriller,arrow_right_thriller,'http://localhost:8000/api/v1/titles/?genre=thriller&sort_by=-imdb_score',"http://localhost:8000/api/v1/titles/?genre=thriller&page=2&sort_by=-imdb_score",thriller_title]
+
+var thriller_array = [thriller,image_thriller,index_thriller,arrow_left_thriller,arrow_right_thriller,
+'http://localhost:8000/api/v1/titles/?genre=thriller&sort_by=-imdb_score',
+"http://localhost:8000/api/v1/titles/?genre=thriller&page=2&sort_by=-imdb_score",thriller_title]
 
 
-let movies = [action_array,animation_array,thriller_array]
-
-
-// Bests Movies and first
-arrow_left = document.getElementById('arrow-left-best')
-arrow_right = document.getElementById('arrow-right-best')
+var movies = [action_array,animation_array,thriller_array,best_array]
 
 fetch(`http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score`)
     .then(reponse => reponse.json())
@@ -69,86 +78,49 @@ fetch(`http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score`)
             .then(rep => rep.json())
             .then(dat => {
                 resume.innerHTML = dat.description
-            })
-
-        //Best Movies
-        for (let i = 1; i < 5; i++) {
-            bests.push(data.results[i].image_url)
-            bests_title.push(data.results[i].title)}
-        })
-    .then(next => {
-        fetch(`http://127.0.0.1:8000/api/v1/titles/?page=2&sort_by=-imdb_score`)
-        .then(reponse => reponse.json())
-        .then(data => {
-            for (let i = 0; i < 3; i++) {
-            bests.push(data.results[i].image_url)
-            bests_title.push(data.results[i].title)}
-    })
-        .then(func => {
-            function change_movies(index_first){
-                if (index_first >= 4 )
-                {
-                    index_first = 3
-                }
-                if (index_first <= 0){
-                    index_first = 0
-                }
-                i = 0
-                for (let img_index = index_first; img_index < index_first + 4;img_index++){
-                    image_best[i].src = bests[img_index]
-                    image_best[i].setAttribute('url',bests[img_index])
-                    image_best[i].setAttribute('index',i+index_first)
-                    i += 1
-                }
-            }
-            function go_left() {
-                if (index_first != 0) {
-                    index_first -= 1
-                }
-                change_movies(index_first)
-            }
-            function go_right() {
-                if (index_first != 3) {
-                    index_first += 1
-                }
-                change_movies(index_first)
-            }
-            change_movies(index_first)
-            arrow_left.onclick = go_left
-            arrow_right.onclick = go_right
-            for (let i = 0; i < image_best.length;i++){
-                image_best[i].addEventListener('click',function(){
-                    fetch(`http://127.0.0.1:8000/api/v1/titles/?title=${bests_title[image_best[i].getAttribute('index')]}`)
-                    .then(reponse => reponse.json())
-                    .then(ans => {
-                        fetch(ans.results[0].url)
-                        .then(res => res.json())
-                        .then(dat => {
-                            popup_display()
-                            const movie = dat
-                            display_informations(movie)
-                            
-                        })
-
-                    })
-                })
-            }
-        })
-    })
+            })})
 
 //Movies
 
-for (let m = 0; m < 3; m++)
+for (let m = 0; m < 4; m++)
 {
     let array_mov = movies[m]
+
+    // Reminder :
+    //      - array_mov[0] --> images from api
+    //      - array_mov[1] --> image places in html
+    //      - array_mov[2] --> index of films on page in category
+    //      - array_mov[3] --> arrow_left
+    //      - array_mov[4] --> arrow_right
+    //      - array_mov[5] --> first page of category
+    //      - array_mov[6] --> second page of category
+    //      - array_mov[7] --> titles of movies in category
+    
     fetch(array_mov[5])
         .then(reponse => reponse.json())
         .then(data => {
+            if (m == 3){
 
-            for (let i = 0; i < 5; i++) {
+                // Remove first movie from bests (already shown)
+                for (let i = 1; i < 5; i++) {
                 array_mov[0].push(data.results[i].image_url)
                 array_mov[7].push(data.results[i].title)}
-            })
+                fetch(array_mov[6])
+                    .then(reponse => reponse.json())
+                    .then(second_page_data => {
+                        for (let i = 0; i < 3; i++) {
+                            array_mov[0].push(second_page_data.results[i].image_url)
+                            array_mov[7].push(second_page_data.results[i].title)}
+                        })
+
+                }
+
+            else{
+                for (let i = 0; i < 5; i++) {
+                array_mov[0].push(data.results[i].image_url)
+                array_mov[7].push(data.results[i].title)}
+            }})
+            
         .then(next => {
         fetch(array_mov[6])
             .then(reponse => reponse.json())
@@ -158,6 +130,9 @@ for (let m = 0; m < 3; m++)
                 array_mov[7].push(data.results[i].title)}
         })
             .then(func => {
+
+                // ------------------Functions to go left and right in sections ------------------
+
                 function change_movies(array_mov){
                     if (array_mov[2] >= 4 )
                     {
@@ -186,6 +161,7 @@ for (let m = 0; m < 3; m++)
                     }
                     change_movies(array_mov)
                 }
+
                 change_movies(array_mov)
                 array_mov[3].onclick = go_left
                 array_mov[4].onclick = go_right
@@ -207,19 +183,25 @@ for (let m = 0; m < 3; m++)
                         })
                     })
                 }
+                
             })
         })
 
 }
 
+
+//--------------------------------- Popup -------------------------------------------------
+
+// Convert movie time to hours
 function convertMinsToHrsMin(minutes) {
   var h = Math.floor(minutes / 60);
   var m = minutes % 60;
   h = h < 10 ?  h : h;
   m = m < 10 ? '0' + m : m;
-  return h + ':' + m;
+  return h + 'h' + m;
 }
 
+// Show Popup
 function popup_display(){
     body[0].style.overflow = "hidden"
     blur.style.top = window.scrollY
@@ -228,12 +210,14 @@ function popup_display(){
     blur.style.visibility = "visible"
 }
 
+// Close Popup
 document.getElementById('close').addEventListener('click',function(){
     popup.style.visibility = 'hidden'
     blur.style.visibility = 'hidden'
     body[0].style.overflow = 'scroll'
 })
 
+// Show informations of movie in Popup
 function display_informations(movie){
     document.getElementById('selection_title').innerHTML = movie.title
     document.getElementById('movie_image').src = movie.image_url
@@ -258,7 +242,7 @@ function display_informations(movie){
         }
     }
     document.getElementById('actors').innerHTML = 'Acteurs : ' + actors
-    document.getElementById('duration').innerHTML = 'Durée : ' + convertMinsToHrsMin(movie.duration) + ' heures'
+    document.getElementById('duration').innerHTML = 'Durée : ' + convertMinsToHrsMin(movie.duration)
     var countries = ''
     for (var ind = 0; ind < movie.countries.length; ind+=1){
         countries += movie.countries[ind]
@@ -276,3 +260,4 @@ function display_informations(movie){
     document.getElementById('resume_popup').innerHTML = "Résumé :<br /><br />" + movie.description
 
 }
+//-----------------------------------------------------------------------------------------------------
